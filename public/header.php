0f6b966ac5a1466ec4755d94c160f6b08819cea6
Tyson Collins <tyson_collinsftd@hotmail.com>
2019-03-26 13:37:12 +1100

    All Backend Complete

---
+++ public/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <title>Tyson's Collection</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <h1>Tyson's collection tracker</h1>
+    <ul>
+        <li><a href="create.php">Add a new artwork</a></li>
+        <li><a href="read.php">Find an artwork</a></li>
+        <li><a href="update.php">Edit an artwork</a></li>
+        <li><a href="delete.php">Delete an artwork</a></li>
+    </ul>
+</body>
---
+++ public/templates/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <title>Tyson's Collection</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <h1>Tyson's collection tracker</h1>
+    <ul>
+        <li><a href="create.php">Add a new artwork</a></li>
+        <li><a href="read.php">Find an artwork</a></li>
+        <li><a href="update.php">Edit an artwork</a></li>
+        <li><a href="delete.php">Delete an artwork</a></li>
+    </ul>
+</body>
